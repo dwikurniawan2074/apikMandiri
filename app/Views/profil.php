a96bed614d8bb7ce3a54eac1159c2b9248e306e3
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-07-20 22:24:19 +0700

    tambah template management

---
+++ app/Views/profil.php
@@ -0,0 +1,256 @@
+<?= $this->extend('template/pageTemplate'); ?>
+<?= $this->section('pageContent'); ?>
+<section class="ftco-section ftco-no-pt ftco-no-pb">
+    <div class="container">
+        <div class="row d-flex no-gutters">
+            <div class="col-md-6 d-flex order-md-last">
+                <div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0" style="background-image:url('<?php echo base_url() ?>/unicare-master/images/about-1.jpg');">
+                </div>
+            </div>
+            <div class="col-md-6 pr-md-5 py-md-5">
+                <div class="heading-section pt-md-5 mb-4">
+                    <span class="subheading">About us</span>
+                    <h2 class="mb-2">Give a helping hand to a needy people</h2>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                    <p><a href="#" class="btn btn-secondary">Become a Volunteer</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section">
+    <div class="container">
+        <div class="row justify-content-center pb-5">
+            <div class="col-md-7 heading-section text-center ftco-animate">
+                <span class="subheading">Welcome to Unicare</span>
+                <h2>We Believe that We Can Save More Lifes with You</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 services-2 w-100 text-center">
+                <div class="icon icon-1 d-flex align-items-center justify-content-center"><span class="flaticon-water-drop"></span></div>
+                <div class="text">
+                    <h4>Water</h4>
+                    <p>Far far away, behind the word mountains, far from the countries.</p>
+                </div>
+            </div>
+            <div class="col-md-3 services-2 w-100 text-center">
+                <div class="icon icon-2 d-flex align-items-center justify-content-center"><span class="flaticon-stethoscope"></span></div>
+                <div class="text">
+                    <h4>Medical</h4>
+                    <p>Far far away, behind the word mountains, far from the countries.</p>
+                </div>
+            </div>
+            <div class="col-md-3 services-2 w-100 text-center">
+                <div class="icon icon-3 d-flex align-items-center justify-content-center"><span class="flaticon-bread"></span></div>
+                <div class="text">
+                    <h4>Food</h4>
+                    <p>Far far away, behind the word mountains, far from the countries.</p>
+                </div>
+            </div>
+            <div class="col-md-3 services-2 w-100 text-center">
+                <div class="icon icon-4 d-flex align-items-center justify-content-center"><span class="flaticon-university"></span></div>
+                <div class="text">
+                    <h4>Education</h4>
+                    <p>Far far away, behind the word mountains, far from the countries.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-counter" id="section-counter">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 mb-5 mb-md-0 text-center text-md-left">
+                <h2 class="font-weight-bold" style="color: #fff; font-size: 22px;">We're on a mission to help all your problems</h2>
+                <a href="#" class="btn btn-white btn-outline-white">Donate Now</a>
+            </div>
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="88984">0</strong>
+                            </div>
+                            <div class="text">
+                                <span>Donation Campaigns are running</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="65000">0</strong>
+                            </div>
+                            <div class="text">
+                                <span>Professional and kind volunteers</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="77000">0</strong>
+                            </div>
+                            <div class="text">
+                                <span>Funds we raised till now on site</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                            <div class="text">
+                                <strong class="number" data-number="50">0</strong>
+                            </div>
+                            <div class="text">
+                                <span>Dedicated Donors</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section testimony-section">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row justify-content-center pb-5">
+            <div class="col-md-7 heading-section heading-section-white text-center ftco-animate">
+                <span class="subheading">Testimony</span>
+                <h2>Happy Clients &amp; Feedbacks</h2>
+            </div>
+        </div>
+        <div class="row ftco-animate">
+            <div class="col-md-12">
+                <div class="carousel-testimony owl-carousel">
+                    <div class="item">
+                        <div class="testimony-wrap d-flex">
+                            <div class="user-img" style="background-image: url(<?php echo base_url() ?>/unicare-master/images/person_1.jpg)">
+                            </div>
+                            <div class="text pl-4">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="fa fa-quote-left"></i>
+                                </span>
+                                <p class="rate">
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </p>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+                                <p class="name">Racky Henderson</p>
+                                <span class="position">Father</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap d-flex">
+                            <div class="user-img" style="background-image: url(<?php echo base_url() ?>/unicare-master/images/person_2.jpg)">
+                            </div>
+                            <div class="text pl-4">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="fa fa-quote-left"></i>
+                                </span>
+                                <p class="rate">
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </p>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+                                <p class="name">Henry Dee</p>
+                                <span class="position">Businesswoman</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap d-flex">
+                            <div class="user-img" style="background-image: url(<?php echo base_url() ?>/unicare-master/images/person_3.jpg)">
+                            </div>
+                            <div class="text pl-4">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="fa fa-quote-left"></i>
+                                </span>
+                                <p class="rate">
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </p>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+                                <p class="name">Mark Huff</p>
+                                <span class="position">Businesswoman</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap d-flex">
+                            <div class="user-img" style="background-image: url(<?php echo base_url() ?>/unicare-master/images/person_4.jpg)">
+                            </div>
+                            <div class="text pl-4">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="fa fa-quote-left"></i>
+                                </span>
+                                <p class="rate">
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </p>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+                                <p class="name">Benjie Busk Jr.</p>
+                                <span class="position">Businesswoman</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimony-wrap d-flex">
+                            <div class="user-img" style="background-image: url(<?php echo base_url() ?>/unicare-master/images/person_1.jpg)">
+                            </div>
+                            <div class="text pl-4">
+                                <span class="quote d-flex align-items-center justify-content-center">
+                                    <i class="fa fa-quote-left"></i>
+                                </span>
+                                <p class="rate">
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                </p>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+                                <p class="name">Ken Bosh</p>
+                                <span class="position">Businesswoman</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-hireme bg-secondary">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-md-8 col-lg-8 d-flex align-items-center">
+                <div class="w-100">
+                    <h2>Best Way to Make a Difference in the Lives of Others</h2>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 d-flex align-items-center justify-content-end">
+                <p class="mb-0"><a href="#" class="btn btn-primary py-3 px-4">Become A Volunteer</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection(); ?>
