153b392efdf75c7130f3f144365c552d6232564b
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-07-26 19:22:45 +0700

    tambah logo dan edit palet warna

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -2,14 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-                <h2 class="footer-heading">Unicare.</h2>
+                <img src="<?php echo base_url()?>/logo_apik.png" alt="logo_apik_mandiri" height="50" width="150" class="footer-heading">
                 <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                <ul class="ftco-footer-social p-0">
-                    <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="fa fa-twitter"></span></a></li>
-                    <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="fa fa-facebook"></span></a></li>
-                    <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="fa fa-instagram"></span></a></li>
-                </ul>
-                <p><a href="#" class="btn btn-quarternary">Donate Now</a></p>
+                <p><a href="#" class="btn btn-donateFooter">Donate Now</a></p>
             </div>
             <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
                 <h2 class="footer-heading">Latest News</h2>
@@ -46,7 +41,7 @@
                 </ul>
             </div>
             <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-                <h2 class="footer-heading">Have a Questions?</h2>
+                <h2 class="footer-heading">Info Kontak</h2>
                 <div class="block-23 mb-3">
                     <ul>
                         <li><span class="icon fa fa-map"></span><span class="text">Jl. Darussalam No.79, Susunan Baru, Kec. Tj. Karang Bar., Kota Bandar Lampung, Lampung 35115</span></li>
@@ -65,7 +60,7 @@
                 <p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     Copyright &copy;<script>
                         document.write(new Date().getFullYear());
-                    </script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+                    </script> Apik Mandiri</a>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
             </div>
         </div>
